1ac39304f01b22ba9ab60ddb3307303682f9f139
marianavns <mariana.vns@gmail.com>
2020-12-09 11:52:59 -0300

    feat(app): adds app.js configurations.

---
+++ src/app.js
@@ -0,0 +1,39 @@
+require('dotenv-safe').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+
+db.on('error', console.log.bind(console, 'connection error'))
+db.once('open', function (){
+  console.log('DB connection successful!')
+})
+
+const index = require('./routes/index')
+const angels = require('./routes/angelsRoute')
+const blesseds = require('./routes/blessedsRoute')
+
+app.use(bodyParser.json());
+
+app.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
+
+app.use('/', index)
+app.use('/teajudo', angels)
+app.use('/precisodeajuda', blesseds)
+
+module.exports = app
